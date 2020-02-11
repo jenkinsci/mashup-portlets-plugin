cf960bb02f50711df6b80dffa2619b623421fe73
georg.henzler <georg.henzler@netcentric.biz>
2020-02-11 11:20:32 +0100

    JENKINS-61039 ID handling compatible with latest dashboard plugin

--- src/main/java/javagh/jenkins/mashupportlets/GenericJsPortlet.java
+++ src/main/java/javagh/jenkins/mashupportlets/GenericJsPortlet.java
@@ -4,6 +4,8 @@ import hudson.Extension;
 import hudson.model.Descriptor;
 import hudson.plugins.view.dashboard.DashboardPortlet;
 
+import java.util.Random;
+
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.bind.JavaScriptMethod;
@@ -16,21 +18,28 @@ import org.kohsuke.stapler.bind.JavaScriptMethod;
  */
 public class GenericJsPortlet extends AbstractMashupPortlet {
 	
+	private static Random generator = new Random(); 
+	  
 	private String javascript;
-	
+
 	private String containerDivId;
 	
 	@DataBoundConstructor
 	public GenericJsPortlet(String name, String javascript) {
 		super(name);
 		this.javascript = javascript;
-		this.containerDivId = "div_genericJs_" + getId();
+		// needed since hudson.plugins.view.dashboard.DashboardPortlet changed its id handling with version 2.12
+		// at configuration change time (on view save and when this code is executed) getId() does not work
+		// properly, it only returns "portlet-" without a number
+		// in genericjsportlet.jelly getId() cannot be used anymore as a frontend ID because it contains a dash now (using containerDivId in all places instead) 
+		int uniqueId = generator.nextInt(32000);
+		this.containerDivId = "genericJs" + uniqueId;
 	}
 	
 	public String getJavascript() {
 		return javascript;
 	}
-
+	
 	public String getContainerDivId() {
 		return containerDivId;
 	}
